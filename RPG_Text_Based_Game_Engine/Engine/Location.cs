4f0b820b5e5a69621d75402c06cbff3773dc1776
grimofdoom <grimofdoom@live.com>
2018-05-22 22:36:58 -0700

    Added Location
    Added Location class, used to define a map.

---
+++ RPG_Text_Based_Game_Engine/Engine/Location.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Text_Based_Game_Engine.Engine {
+    public class Location {
+        public String name;
+        public List<List<List<Command>>> spots = new List<List<List<Command>>>(); 
+        
+        public void AddSpot(int x, int y, Command z) {
+            spots[x][y].Add(z);
+        }
+
+        public List<Command> GrabSpot(int x, int y) {
+            List<Command> tempList = new List<Command>();
+            tempList = spots[x][y];
+            return tempList;
+        }
+    }
+}
